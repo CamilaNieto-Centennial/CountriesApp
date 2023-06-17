af820a5ba50b37e1c4f365e38840bb3f16c2a8ac
CamilaNieto-Centennial <fourcamilitanieto@gmail.com>
2023-06-16 19:51:09 -0500

    feat: ADD Initial Content & Mantine Styles

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import { MantineProvider, Container , Flex, Title, Text, Button, Paper } from '@mantine/core';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <MantineProvider
+      theme={{
+        fontFamily: 'Open Sans, sans-serif',
+        headings: { fontFamily: 'Greycliff CF, sans-serif' },
+        components: {
+          Button: {
+            styles: {
+              root: { fontSize: 18 },
+            },
+          },
+        },
+      }}
+    >
+    <Container fluid py="sm">
+      <Title order={1} mb="xs">Countries App</Title>
+      <Flex
+        mih={50}
+        gap="md"
+        px = "0em"
+        justify="space-between"
+        align="center"
+        direction="row"
+        wrap="wrap"
+        mb="xs"
+      >
+        <Flex
+          gap="md"
+          wrap="wrap"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          {/*<Button>All Data</Button>*/}
+          <Button color='blue.9'>Smaller than Lithuania</Button>
+          <Button color='blue.9'>In Oceania</Button>
+        </Flex>
+        <Button color='teal.9' uppercase >Submit</Button>
+      </Flex>
+      <Paper shadow="sm" radius="sm" p="xs" mb="xs" withBorder>
+          <Title order={3}>Country Name</Title>
+          <Text>Region</Text>
+          <Text>Area</Text>
+      </Paper>
+      <Paper shadow="sm" radius="sm" p="xs" withBorder>
+          <Title order={3}>Country Name</Title>
+          <Text>Region</Text>
+          <Text>Area</Text>
+      </Paper>
+    </Container>
+    </MantineProvider>
   );
 }
 
