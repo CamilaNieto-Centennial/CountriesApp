131fbe0741492c9b655de83955a45f5033738414
CamilaNieto-Centennial <fourcamilitanieto@gmail.com>
2023-06-18 21:04:33 -0500

    style: Fix styling issues

--- src/common/ThemeSwitcher/ThemeSwitcher.jsx
+++ src/common/ThemeSwitcher/ThemeSwitcher.jsx
@@ -38,7 +38,7 @@ export const ThemeSwitcher = () => {
     const largeScreen = useMediaQuery('(min-width: 26em)');
 
     return (
-        <Group position="center" mt={largeScreen ? 'xl' : 'sm'} mb={largeScreen ? 'xl' : 'lg'}>
+        <Group position="center" mt={largeScreen ? 'xl' : '0'} mb={largeScreen ? 'xl' : 'lg'}>
             <UnstyledButton
                 aria-label="Toggle theme"
                 className={classes.control}
